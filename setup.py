b925c1406f47c2a797c1ec3db06c5320d7366d6a
James R. Gregg <james.r.greggaz68@gmail.com>
2025-01-14 13:27:40 -0700

    Initial commit from your app

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my-app',
     version='1.0',
-    packages=['my_app'],
+    packages=find_packages(),
     install_requires=['Flask', 'requests'],
     tests_require=['unittest'],
     test_suite='tests',
